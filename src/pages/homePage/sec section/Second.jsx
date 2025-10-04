9c97e589c9f6b9ded5ef72af7d40cf80a4bb0eb7
arosha ranasinghe <120770513+Arosha-Ranasinghe@users.noreply.github.com>
2025-10-05 00:40:56 +0530

    bye

--- src/pages/homePage/sec section/Second.jsx
+++ src/pages/homePage/sec section/Second.jsx
@@ -1,34 +1,7 @@
 import React from "react";
 import "./Second.css";
-import { useEffect } from "react";
 
 function Second() {
-  useEffect(() => {
-    const options = {
-      root: null, // Use the viewport as the root
-      rootMargin: "0px",
-      threshold: 0.5, // Trigger when 50% of the element is in view
-    };
-
-    const observer = new IntersectionObserver((entries, observer) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          // Add the 'animate' class when the section is in view
-          entry.target.classList.add("animate");
-          observer.unobserve(entry.target); // Stop observing after animation trigger
-        }
-      });
-    }, options);
-
-    // Start observing the .home-aboutus-info section
-    const section = document.querySelector(".home-aboutus-info");
-    observer.observe(section);
-
-    return () => {
-      observer.disconnect(); // Clean up the observer on component unmount
-    };
-  }, []);
-
   return (
     <div className="home-aboutus">
       <div className="home-aboutus-info">
